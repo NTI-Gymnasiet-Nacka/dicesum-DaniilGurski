a70f6d92efcb37eb74e65640542093ca19bbdf74
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-21 10:32:33 +0200

    done.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
-
+# Författare: Daniil
+# Datum: 2024 Onsdag 21
 def main():
-    user_input = input().split(" ")
+    user_input = input("").split(" ")
+    max_dice = int(max(user_input))
+    min_dice = int(min(user_input))
+
+    for sum in range(min_dice + 1, max_dice + 2):
+        print(sum)
+
 
 if __name__ == "__main__":
     main()
