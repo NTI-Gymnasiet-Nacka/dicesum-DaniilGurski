ce0d75cb2e04e3670de191026adeda6c1c5ef6ba
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-21 10:40:06 +0200

    done.

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 # Författare: Daniil
 # Datum: 2024 Onsdag 21
 def main():
-    user_input = input("").split(" ")
-    max_dice = int(max(user_input))
-    min_dice = int(min(user_input))
+    user_input = [int(x) for x in input("").split(" ")]
+    min_dice = min(user_input)
+    max_dice = max(user_input)
 
     for sum in range(min_dice + 1, max_dice + 2):
         print(sum)
-
 
 if __name__ == "__main__":
     main()
