2252429211d4d388ffaba8407be1a748983fa525
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-21 10:48:15 +0200

    comment.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Dice sum probability calculator
 # Författare: Daniil
 # Datum: 2024 Onsdag 21
+
 def main():
     user_input = [int(x) for x in input("").split(" ")]
     min_dice = min(user_input)
     max_dice = max(user_input)
 
+    # EX: min: 4, max: 6
+    # Loopen körs från 5 till 7 vilket är våra mest sannolika summorna 
     for sum in range(min_dice + 1, max_dice + 2):
         print(sum)
 
